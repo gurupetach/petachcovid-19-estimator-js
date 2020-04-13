8f93c707118ae0923c1ccd8ea9f14d21822a732c
guru petach <peterachieng4@gmail.com>
2020-04-14 01:38:58 +0300

    start building estimatoe

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,44 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const  {
+    region : {
+        avgDailyIncomeInUsd
+    },
+    reportedCases,
+    timeToElapse,
+    periodType,
+    population,
+    totalHospitalBeds
+
+    } = data;
+    
+    const impact = {};
+    const severImpact = {};
+
+    impact.currentlyInfected = Math.trunc(reportedCases * 10);
+    severImpact.currentlyInfected = Math.trunc(reportedCases * 50);
+
+    // check if time elapsed is months, weeks,days
+
+    let timeFactor;
+    
+    switch (periodType.trim().toLowerCase()){
+        case 'months':
+            timeFactor = Math.trunc((timeToElapse * 30) / 3);
+            break;
+
+         case 'weeks':
+                timeFactor = Math.trunc((timeToElapse * 7) / 3);
+                break;
+
+         case 'days':
+            timeFactor = Math.trunc((timeToElapse) / 3);
+            break;
+            default:
+    }
+
+    //time passed as infection rates grow
+    impact.infectionsByRequestTime = impact.currentlyInfected * (2 ** timeFactor);
+    severeImpact.infectionsByRequestTime = severImpact.currentlyInfected * (2 ** timeFactor);
+}
 
 export default covid19ImpactEstimator;
